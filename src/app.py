15f17e18c4b5fd5dc9ae55f1531edd1c172e5282
Sebastian Wagner <swagner-de@users.noreply.github.com>
2017-06-16 23:28:42 +0200

    implemented user auth

--- src/app.py
+++ src/app.py
@@ -1,15 +1,25 @@
 from flask import Flask
-from model import db
-from flask_login import LoginManager
-
+from model import db, User
+from flask_httpauth import HTTPBasicAuth
 
 # Create application
 app = Flask('Recodingaviation')
 
 # MongoDB settings
 app.config.from_pyfile('app.cfg')
-lm = LoginManager
+
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_pw(username, password):
+    return User.check_pw(username, password)
+
 db.init_app(app)
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, %s!" % auth.username()
 
 
 if __name__ == "__main__":
--- src/test.py
+++
@@ -1,9 +0,0 @@
-from model import User
-
-def main():
-    u = User(username='seb', password='somepass')
-    u.save()
-
-
-if __name__ == '__main__':
-    main()
