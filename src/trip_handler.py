a884031ef8a8c5b8680beb5da55254ce0277ed6f
Sebastian Wagner <swagner-de@users.noreply.github.com>
2017-06-17 16:43:25 +0200

    implemented delays and pedestrian time

--- src/trip_handler.py
+++ src/trip_handler.py
@@ -14,6 +14,14 @@ def get_connection(leg, legs_remaining):
                 earliest = r.departure_date
     return {'inbound': leg, 'outbound': con} if con else None
 
+def get_flight_oper_info(connection):
+    connection['inbound'].get_operational_info()
+    connection['outbound'].get_operational_info()
+    return connection
+
+def get_transfer_info(connection):
+    from ams_wayfinding import get_directions_from_gates
+    connection['transfer'] = get_directions_from_gates(arr_gate=connection['inbound'].gate, dep_gate=connection['outbound'].gate)
 
 def get_current_connect_info(user):
     current_legs = user.get_current_legs()
@@ -21,4 +29,9 @@ def get_current_connect_info(user):
         connection = get_connection(current_legs[0], current_legs[1:])
     except IndexError:
         return None
+    from pprint import pprint
+    pprint(connection)
+    get_flight_oper_info(connection)
+    get_transfer_info(connection)
     return connection
+
