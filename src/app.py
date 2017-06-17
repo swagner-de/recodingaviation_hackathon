75f3814fe07afb29932b2cc6b513a2c49872f815
ivogoman <ivo.gosemann@gmail.com>
2017-06-17 23:35:29 +0200

    Cross Origin

--- src/app.py
+++ src/app.py
@@ -49,6 +49,13 @@ def get_bag_journey():
     from trip_handler import get_bags_info
     return jsonify(get_bags_info(user=u))
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000)
